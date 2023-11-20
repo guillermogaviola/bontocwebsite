737e0f6e2b9ee0c7f0cfffe31b409350f3081ff7
Guillermo <guillermosumamponggaviola@gmail.com>
2023-11-20 14:57:45 +0800

    added new 11/20/23

--- routes/web.php
+++ routes/web.php
@@ -47,20 +47,21 @@ Route::group(['namespace' => 'App\Http\Controllers'], function()
 
 
      Route::group(['middleware' => ['guest']], function() {
+
         //Logout Routes
             Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+
+        //Login Routes
+            Route::get('/login', 'LoginController@show')->name('login.show');
+            Route::post('/login', 'LoginController@login')->name('login.perform');
          
      });
 
     Route::group(['middleware' => ['auth'],'prefix'=> 'admin'], function() {
 
-         //Register Routes
+        //Register Routes
             Route::get('/register', 'RegisterController@show')->name('register.show');
             Route::post('/register', 'RegisterController@register')->name('register.perform');
-
-            //Login Routes
-            Route::get('/login', 'LoginController@show')->name('login.show');
-            Route::post('/login', 'LoginController@login')->name('login.perform');
 
         // Dashboard
         Route::get('/dashboard', 'DashboardController@index')->name('admin.dashboard');

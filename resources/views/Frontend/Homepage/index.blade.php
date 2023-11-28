a7e351adc21ffce41a1561cb948211f098fa3867
Guillermo <guillermosumamponggaviola@gmail.com>
2023-11-28 13:14:32 +0800

    112823

--- resources/views/Frontend/Homepage/index.blade.php
+++ resources/views/Frontend/Homepage/index.blade.php
@@ -87,7 +87,7 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="d-flex align-items-center justify-content-between bg-light py-2 px-4 mb-3">
-                                <h3 class="m-0"></h3>
+                                <h3 class="m-0">World Mental Health Month</h3>
                                 <a class="text-secondary font-weight-medium text-decoration-none" href="">View All</a>
                             </div>
                         </div>
@@ -106,8 +106,6 @@
                                 </div>
                             </div>
                         @endforeach
-
-                    
                         <div class="col-lg-2">
                             <div class="position-relative mb-3">
                                 <img class="img-fluid w-100" src="{{ asset('resources/img/WorldMentalHealthMonth/395316821_303000189163725_2409139856403516309_n.jpg') }}" style="object-fit: cover;">
@@ -118,7 +116,58 @@
                                         <span>October 27, 2023</span>
                                     </div>
                                     <a class="h6" href="">Bontoc, October 26, 2023 - The Municipality of Bontoc, under the</a>
-                                    <p class="m-0">The launching was a day of enlightenment, where residents of Bontoc came together to learn about the importance of Mental Health and the resources available to them. It marked a significant milestone in the town's journey towards creating a society that understands, supports, and uplifts its members.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <div class="position-relative mb-3">
+                                <img class="img-fluid w-100" src="{{ asset('resources/img/WorldMentalHealthMonth/395316821_303000189163725_2409139856403516309_n.jpg') }}" style="object-fit: cover;">
+                                <div class="overlay position-relative bg-light">
+                                    <div class="mb-2" style="font-size: 14px;">
+                                        <a href="">World Mental Health Month</a>
+                                        <span class="px-1">/</span>
+                                        <span>October 27, 2023</span>
+                                    </div>
+                                    <a class="h6" href="">Bontoc, October 26, 2023 - The Municipality of Bontoc, under the</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <div class="position-relative mb-3">
+                                <img class="img-fluid w-100" src="{{ asset('resources/img/WorldMentalHealthMonth/395316821_303000189163725_2409139856403516309_n.jpg') }}" style="object-fit: cover;">
+                                <div class="overlay position-relative bg-light">
+                                    <div class="mb-2" style="font-size: 14px;">
+                                        <a href="">World Mental Health Month</a>
+                                        <span class="px-1">/</span>
+                                        <span>October 27, 2023</span>
+                                    </div>
+                                    <a class="h6" href="">Bontoc, October 26, 2023 - The Municipality of Bontoc, under the</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <div class="position-relative mb-3">
+                                <img class="img-fluid w-100" src="{{ asset('resources/img/WorldMentalHealthMonth/395316821_303000189163725_2409139856403516309_n.jpg') }}" style="object-fit: cover;">
+                                <div class="overlay position-relative bg-light">
+                                    <div class="mb-2" style="font-size: 14px;">
+                                        <a href="">World Mental Health Month</a>
+                                        <span class="px-1">/</span>
+                                        <span>October 27, 2023</span>
+                                    </div>
+                                    <a class="h6" href="">Bontoc, October 26, 2023 - The Municipality of Bontoc, under the</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <div class="position-relative mb-3">
+                                <img class="img-fluid w-100" src="{{ asset('resources/img/WorldMentalHealthMonth/395316821_303000189163725_2409139856403516309_n.jpg') }}" style="object-fit: cover;">
+                                <div class="overlay position-relative bg-light">
+                                    <div class="mb-2" style="font-size: 14px;">
+                                        <a href="">World Mental Health Month</a>
+                                        <span class="px-1">/</span>
+                                        <span>October 27, 2023</span>
+                                    </div>
+                                    <a class="h6" href="">Bontoc, October 26, 2023 - The Municipality of Bontoc, under the</a>
                                 </div>
                             </div>
                         </div>
@@ -132,7 +181,6 @@
                                         <span>October 27, 2023</span>
                                     </div>
                                     <a class="h6" href="">Bontoc, October 26, 2023 - The Municipality of Bontoc, under the</a>
-                                    <p class="m-0"> The Municipality of Bontoc's commitment to mental health and the launch of Project NOEL are testament to the town's dedication to the holistic well-being of its people. It is a significant step forward in transforming how mental health is perceived and addressed within the community, making Bontoc a leader in championing mental well-being.</p>
                                 </div>
                             </div>
                         </div>
@@ -152,7 +200,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">Talisay, Bontoc, Southern Leyte - A momentous...</a>
-                                    <p class="m-0">...occasion unfolded to celebrate the grand opening of the Notarial Office of Atty. Mae Angieline T. Salva-Alaras located in the heart of Brgy. Talisay, Bontoc Southern Leyte. Atty. Alaras, who is also a Certified Public Accountant is proud as well as humbled to serve and offer her legal services to the citizenry of Bontoc and its neighboring towns in Southern Leyte.</p>
                                 </div>
                             </div>
                         </div>
@@ -166,7 +213,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">Talisay, Bontoc, Southern Leyte - A momentous...</a>
-                                    <p class="m-0">The inauguration of the law office was attended by esteemed dignitaries, including the First Lady of Bontoc, Dr. Aileen D. Alinsub, who graced the event on behalf of Mayor Noel E. Alinsub as a representative of the community.</p>
                                 </div>
                             </div>
                         </div>
@@ -180,7 +226,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">Talisay, Bontoc, Southern Leyte - A momentous...</a>
-                                    <p class="m-0">She expressed her wholehearted support for the legal endeavors and growth in the town. She highlighted the significant role that legal professionals play in ensuring justice, fairness, and the rule of law within the community.</p>
                                 </div>
                             </div>
                         </div>
@@ -194,7 +239,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">Talisay, Bontoc, Southern Leyte - A momentous...</a>
-                                    <p class="m-0">It was also attended by the Parish Priest of the Bontoc, Rev. Fr. Joselito Catubig who performed the holy blessing of the notarial office.</p>
                                 </div>
                             </div>
                         </div>
@@ -208,7 +252,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">Talisay, Bontoc, Southern Leyte - A momentous...</a>
-                                    <p class="m-0">Atty. Alaras, expressed her gratitude to everyone present and shared her vision for the law office. She emphasized that in response to the call for entrepreneurs and investors in her hometown, she decided to put up her office and services in the area to serve the legal needs of the people of Bontoc, Southern Leyte.</p>
                                 </div>
                             </div>
                         </div>
@@ -222,7 +265,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">Talisay, Bontoc, Southern Leyte - A momentous...</a>
-                                    <p class="m-0">Atty. Mae Angieline T. Salva-Alaras’ law office will undoubtedly contribute to the continued growth and development of Brgy. Talisay and the surrounding areas of the Municipality of Bontoc.</p>
                                 </div>
                             </div>
                         </div>
@@ -243,7 +285,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">In a heartwarming event that brought joy and unity to...</a>
-                                    <p class="m-0">the Municipality of Bontoc, a successful Mass Wedding named "Kasalan ng Bayan 2023" was held on October 31, 2023, just a day after the Barangay elections. The event, organized by the dedicated efforts of Ms. Ruby Grace Nidoy, aimed to legalize the unions of 16 unmarried couples, further reinforcing the sense of togetherness and commitment within the community.</p>
                                 </div>
                             </div>
                         </div>
@@ -257,7 +298,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">In a heartwarming event that brought joy and unity to...</a>
-                                    <p class="m-0">The Mass Wedding ceremony was a resounding success, with couples from all walks of life coming together to affirm their love and dedication to each other. It was a remarkable celebration of love and unity, symbolizing the strength of the Bontoc community.</p>
                                 </div>
                             </div>
                         </div>
@@ -271,7 +311,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">In a heartwarming event that brought joy and unity to...</a>
-                                    <p class="m-0">Mayor Noel E. Alinsub, who is known for his strong support for community events and initiatives, graced the occasion with his presence. He was joined by his wife, Dr. Aileen D. Alinsub, the First Lady of Bontoc, who also played an instrumental role in making the event a grand success.</p>
                                 </div>
                             </div>
                         </div>
@@ -285,7 +324,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">In a heartwarming event that brought joy and unity to...</a>
-                                    <p class="m-0">During the ceremony, Mayor Noel E. Alinsub delivered a heartwarming speech, emphasizing the significance of love and unity within the community. He commended the initiative of Ms. Ruby Grace Nidoy and all those involved in organizing the Mass Wedding, highlighting how such events promote strong family ties and community bonds.</p>
                                 </div>
                             </div>
                         </div>
@@ -299,8 +337,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">In a heartwarming event that brought joy and unity to...</a>
-                                    <p class="m-0">Dr. Aileen D. Alinsub, whose dedication to the welfare of the community is widely recognized, also shared her words of encouragement with the couples. She expressed her support for their commitment and highlighted the importance of love in building a strong and resilient society.
-                                    The "Kasalan ng Bayan 2023" was more than just a ceremony; it was a celebration of love, commitment, and community. It sent a powerful message that love knows no boundaries and that the bonds of marriage are a foundation for the unity and prosperity of the Municipality of Bontoc.</p>
                                 </div>
                             </div>
                         </div>
@@ -314,8 +350,6 @@
                                         <span>November 6, 2023</span>
                                     </div>
                                     <a class="h6" href="">In a heartwarming event that brought joy and unity to...</a>
-                                    <p class="m-0">The event was marked by the exchange of vows, rings, and heartfelt promises among the couples, witnessed by their families and friends. The joy and love that filled the air were palpable, leaving a lasting impression on all who attended.
-                                    The success of "Kasalan ng Bayan 2023" is a testament to the strong sense of community and the unwavering commitment to creating a better future for the Municipality of Bontoc. It was a day of celebration, unity, and the realization that love and togetherness are essential values that bind the community together.</p>
                                 </div>
                             </div>
                         </div>
